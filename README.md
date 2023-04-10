a70a974fcd232ddc2de968d51eef11e0cecfea1e
ujan <ujandev@gmail.com>
2023-04-10 18:46:01 +0530

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+wav2tab
+==============================
+
+music to tab generation
+
+# Demucs installation #
+# from https://github.com/facebookresearch/demucs
+
+git clone https://github.com/facebookresearch/demucs.git
+cd demucs
+conda env update -f environment-cpu.yml  # if you don't have GPUs
+conda env update -f environment-cuda.yml # if you have GPUs
+conda activate demucs
+pip install -e .
+
+brew install sound-touch  # mac
+sudo apt-get install soundstretch  # ubuntu
+
+# for ffmpeg error
+conda uninstall ffmpeg
+conda install -c conda-forge ffmpeg
+
+# basic pitch installation #
+# from https://github.com/spotify/basic-pitch
+
+pip install basic-pitch --upgrade
+
---
+++ experiments/python/wav2notes_demo.py
@@ -0,0 +1,114 @@
+from argparse import ArgumentParser
+import io
+from pathlib import Path
+import select
+from shutil import rmtree
+import subprocess as sp
+import sys
+from typing import Dict, Tuple, Optional, IO
+from os.path import dirname, abspath
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
+
+# get root directory
+#root = abspath(__file__)
+#while root.split('/')[-1] != 'wav2tab':
+    #root = dirname(root)
+
+# hyps #
+
+model = "htdemucs"
+extensions = ["mp3", "wav", "ogg", "flac"]  # we will look for all those file types.
+two_stems = None   # only separate one stems from the rest, for instance
+# two_stems = "vocals"
+
+# Options for the output audio.
+mp3 = True
+mp3_rate = 320
+float32 = False  # output as float 32 wavs, unsused if 'mp3' is True.
+int24 = False    # output as int24 wavs, unused if 'mp3' is True.
+# You cannot set both `float32 = True` and `int24 = True` !!
+
+def find_files(in_path):
+    out = []
+    for file in Path(in_path).iterdir():
+        if file.suffix.lower().lstrip(".") in extensions:
+            out.append(file)
+    return out
+
+
+def copy_process_streams(process: sp.Popen):
+    def raw(stream: Optional[IO[bytes]]) -> IO[bytes]:
+        assert stream is not None
+        if isinstance(stream, io.BufferedIOBase):
+            stream = stream.raw
+        return stream
+
+    p_stdout, p_stderr = raw(process.stdout), raw(process.stderr)
+    stream_by_fd: Dict[int, Tuple[IO[bytes], io.StringIO, IO[str]]] = {
+        p_stdout.fileno(): (p_stdout, sys.stdout),
+        p_stderr.fileno(): (p_stderr, sys.stderr),
+    }
+    fds = list(stream_by_fd.keys())
+
+    while fds:
+        # `select` syscall will wait until one of the file descriptors has content.
+        ready, _, _ = select.select(fds, [], [])
+        for fd in ready:
+            p_stream, std = stream_by_fd[fd]
+            raw_buf = p_stream.read(2 ** 16)
+            if not raw_buf:
+                fds.remove(fd)
+                continue
+            buf = raw_buf.decode()
+            std.write(buf)
+            std.flush()
+
+
+def separate(inp=None, outp=None):
+    inp = inp 
+    outp = outp 
+    cmd = ["python3", "-m", "demucs.separate", "-o", str(outp), "-n", model]
+    if mp3:
+        cmd += ["--mp3", f"--mp3-bitrate={mp3_rate}"]
+    if float32:
+        cmd += ["--float32"]
+    if int24:
+        cmd += ["--int24"]
+    if two_stems is not None:
+        cmd += [f"--two-stems={two_stems}"]
+    files = [str(f) for f in find_files(inp)]
+    if not files:
+        print(f"No valid audio files in {inp}")
+        return
+    print("Going to separate the files:")
+    print('\n'.join(files))
+    print("With command: ", " ".join(cmd))
+    p = sp.Popen(cmd + files, stdout=sp.PIPE, stderr=sp.PIPE)
+    copy_process_streams(p)
+    p.wait()
+    if p.returncode != 0:
+        print("Command failed, something went wrong.")
+
+
+if __name__ == '__main__':
+
+    argp = ArgumentParser()
+
+    # song input
+    argp.add_argument('--in_path', type=str, default=None)
+    # stems output
+    argp.add_argument('--out_path', type=str, default='./')
+
+    # parse and check cli arguments #
+    args = argp.parse_args() 
+
+    # separate into stems
+    #separate(args.in_path, args.out_path)
+
+    model_output, midi_data, note_events = predict("/users/ujan/wav2tab/experiments/python/htdemucs/thunderstruck/other.mp3")
+
+    print(midi_data)
+
+    
+
